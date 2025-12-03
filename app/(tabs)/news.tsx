0c47d3333423875d0019220cc63ebb3d3e25634c
santhoshd4gd <santhoshd@4gd.ai>
2025-12-04 00:43:14 +0530

    first

--- app/(tabs)/news.tsx
+++ app/(tabs)/news.tsx
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function HomeScreen() {
+export default function NewsScreen() {
   return (
     <View style={styles.container}>
       <Text>Coming Soon</Text>
